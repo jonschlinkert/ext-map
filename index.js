ef39dea522ab2d2bdd300d5be6879cde6155aa3a
jonschlinkert <dev@sellside.com>
2014-11-13 11:08:56 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  // html
+  '.md': '.html',
+  '.hbs': '.html',
+  '.swig': '.html',
+  '.tmpl': '.html',
+  '.html': '.html',
+  '.htm': '.html',
+  // css
+  '.less': '.css',
+  '.styl': '.css',
+  '.sass': '.css',
+  '.scss': '.css',
+  '.css': '.css',
+  // js
+  '.coffee': '.js',
+  '.js': '.js',
+};
